0589c1a034b50d97b290c735ac1efca18b22e9e1
cluab <cluab11222@gmail.com>
2022-11-07 16:56:19 +0200

    added changes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import './style.css';
-import Store from './modules/locolstorage.js';
-import ToDoInfo from './modules/add.js';
-import { check } from './modules/status.js';
+import "./style.css";
+import Store from "./modules/locolstorage.js";
+import ToDoInfo from "./modules/add.js";
+import { check } from "./modules/status.js";
 
 // added input to active event listener
-const input = document.getElementById('input');
-document.addEventListener('DOMContentLoaded', ToDoInfo);
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    if (input.value === '') return;
+const input = document.getElementById("input");
+document.addEventListener("DOMContentLoaded", ToDoInfo);
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    if (input.value === "") return;
     event.preventDefault();
     const addedwork = input.value;
-    const retrieveData = localStorage.getItem('list');
+    const retrieveData = localStorage.getItem("list");
     // created the index for Local storage
-    let ind = 1;
+    let indexs = 1;
     if (retrieveData) {
       const previousData = JSON.parse(retrieveData);
       for (let i = 0; i < previousData.length; i += 1) {
-        ind += 1;
+        indexs += 1;
       }
     }
-    const list = new ToDoInfo(addedwork, ind);
+    const list = new ToDoInfo(addedwork, indexs);
     Store.getlist();
     Store.addlist(list);
     ToDoInfo.addToList(list);
     // empty's the value of the input
-    input.value = '';
+    input.value = "";
   }
 });
 
 // retrieves any data from local storage
-const retrieveData = localStorage.getItem('list');
-const body = document.querySelector('body');
+const retrieveData = localStorage.getItem("list");
+const body = document.querySelector("body");
 body.onload = () => {
   if (retrieveData) {
     const previousData = JSON.parse(retrieveData);
@@ -43,7 +43,7 @@ body.onload = () => {
 };
 
 // actives the clear button when it clicked
-document.querySelector('.clear').addEventListener('click', () => {
+document.querySelector(".clear").addEventListener("click", () => {
   ToDoInfo.deletetask();
   Store.removework();
 });
